821c1944cc4288140b53ce45af2f8d3f74952d35
Gabriel Oliveira <goliveiracod@gmail.com>
2020-10-28 17:47:56 -0300

    added exception handler

---
+++ src/main/java/br/edu/ifsp/arq/goliveiracod/school/config/exception/CustomRestDefaultExceptionHandler.java
@@ -0,0 +1,40 @@
+package br.edu.ifsp.arq.goliveiracod.school.config.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class CustomRestDefaultExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex
+            , HttpHeaders headers
+            , HttpStatus status
+            , WebRequest request
+    ) {
+        List<String> errors = new ArrayList<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(
+                error -> errors.add(error.getField() + ": " + error.getDefaultMessage())
+        );
+
+        ex.getBindingResult().getGlobalErrors().forEach(
+                error -> errors.add(error.getObjectName() + ": " + error.getDefaultMessage())
+        );
+
+        ApiError apiError =
+                new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), errors);
+        
+        return handleExceptionInternal(
+                ex, apiError, headers, apiError.getStatus(), request);
+    }
+}
---
+++ src/main/java/br/edu/ifsp/arq/goliveiracod/school/config/exception/NotFoundExceptionHandler.java
@@ -0,0 +1,19 @@
+package br.edu.ifsp.arq.goliveiracod.school.config.exception;
+
+import br.edu.ifsp.arq.goliveiracod.school.exception.ProgramNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class NotFoundExceptionHandler {
+    @ExceptionHandler({ProgramNotFoundException.class})
+    public ResponseEntity<Object> handleAll(Exception ex) {
+        ApiError apiError = new ApiError(
+                HttpStatus.NOT_FOUND, ex.getLocalizedMessage(), "error: " + ex.getLocalizedMessage());
+        return new ResponseEntity<>(
+                apiError, new HttpHeaders(), apiError.getStatus());
+    }
+}
